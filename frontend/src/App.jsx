0f934af7b06bb3e59c8a8713b737041ab674c017
racha1125 <akshay25racha@gmail.com>
2025-05-11 17:16:01 +0530

    User Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
 import Home from './pages/Home'
 import {Toaster} from "sonner";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import CollectionPage from './pages/CollectionPage';
 function App() {
   return (
     <Router>
@@ -10,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home/>} />
+          <Route path='login' element={<Login/>}/>
+          <Route path='register' element={<Register/>}/>
+          <Route path='profile' element={<Profile/>}/>
+          <Route path='collections/:collection' element={<CollectionPage/>}/>
         </Route>
       </Routes>
     </Router>
