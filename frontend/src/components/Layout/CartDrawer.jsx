0e077c9d1141771c1208846b7e64901ab7758feb
racha1125 <akshay25racha@gmail.com>
2025-05-22 21:19:54 +0530

    Cart

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { IoMdClose } from 'react-icons/io';
 import CartContents from '../Cart/CartContents';
+import { useNavigate } from 'react-router-dom';
 
 function CartDrawer({ drawerOpen, toogleCartDrawer }) {
+  const navigate = useNavigate();
+  const handleCheckout = () => {
+    toogleCartDrawer();
+    navigate("/checkout");
+  };
+
   return (
     <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
       {/* Close Button */}
@@ -23,7 +30,7 @@ function CartDrawer({ drawerOpen, toogleCartDrawer }) {
       </div>
       {/* Check Out Button */}
       <div className='p-4 bg-white sticky bottom-0'>
-        <button className='w-full bg-black text-white py-2 rounded-lg font-semibold hover:bg-gray-800 cursor-pointer' >Checkout</button>
+        <button onClick={handleCheckout} className='w-full bg-black text-white py-2 rounded-lg font-semibold hover:bg-gray-800 cursor-pointer' >Checkout</button>
         <p>
           Shipping, taxes, and discounts will be calculated at checkout.
         </p>
