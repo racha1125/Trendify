41f80f2b4feb4dcd7825ddb9e6da5e3e04c4116f
racha1125 <akshay25racha@gmail.com>
2025-05-22 21:21:01 +0530

    Orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,9 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import CollectionPage from './pages/CollectionPage';
 import ProductDetails from './components/Products/ProductDetails';
+import Checkout from './components/Cart/Checkout';
+import OrderConfirmationPage from './pages/OrderConfirmationPage';
+import MyOrdersPage from './pages/MyOrdersPage';
 function App() {
   return (
     <Router>
@@ -20,6 +23,9 @@ function App() {
           <Route path='profile' element={<Profile/>}/>
           <Route path='collections/:collection' element={<CollectionPage/>}/>
           <Route path='product/:id' element={<ProductDetails/>}/>
+          <Route path="checkout/" element={<Checkout/>}/>
+          <Route path="order-confirmation" element={<OrderConfirmationPage/>}/>
+          <Route path="my-orders/" element={<MyOrdersPage/>}/>
         </Route>
       </Routes>
     </Router>
