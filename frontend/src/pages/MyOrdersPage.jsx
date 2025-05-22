4cd9dfec8d1280bac73b8b117c38d8ad0d3e475a
racha1125 <akshay25racha@gmail.com>
2025-05-22 21:20:31 +0530

    Order Details

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function MyOrdersPage() {
     const [orders,setOrders] = useState([]);
+    const navigate = useNavigate();
     useEffect(()=> {
         setTimeout(() =>{
             const mockOrders = [
@@ -35,6 +37,9 @@ function MyOrdersPage() {
             setOrders(mockOrders);
         },1000);
     },[]);
+    const handleRowClick = (orderId) => {
+        navigate(`/order/${orderId}`);
+    }
   return (
     <div className='max-w-7xl mx-auto p-4 sm:p-6'>
         <h2 className='text-xl sm:text-2xl font-bold'>My Orders</h2>
@@ -54,7 +59,10 @@ function MyOrdersPage() {
                 <tbody>
                     {orders.length > 0 ? (
                         orders.map((order)=>(
-                            <tr key={order._id} className='border-b hover:border-gray-50 cursor-pointer'>
+                            <tr 
+                                key={order._id} 
+                                onClick={()=> handleRowClick(order._id)}
+                                className='border-b hover:border-gray-50 cursor-pointer'>
                                 <td className='py-2 px-2 sm:py-4 sm:px-4'>
                                     <img 
                                         src={order.orderItems[0].image} alt={order.orderItems[0].name} 
