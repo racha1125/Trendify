d112cdc51a5e6cbd62c26be9c4634ea8e9b3f0e4
racha1125 <akshay25racha@gmail.com>
2025-05-25 10:25:11 +0530

    Product Routes for best-sellers, new-arrivals.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -201,8 +201,8 @@ router.get("/", async (req, res) => {
 
         if(minPrice || maxPrice) {
             query.price = {};
-            if(minPrice) query.price.gte = Number(minPrice);
-            if(maxPrice) query.price.lte = Number(maxPrice);
+            if(minPrice) query.price.$gte = Number(minPrice);
+            if(maxPrice) query.price.$lte = Number(maxPrice);
         }
 
         if (search) {
@@ -239,6 +239,79 @@ router.get("/", async (req, res) => {
         console.error(error);
         res.status(500).send("Server Error");
     }
-})
+});
+
+// @route GET /api/products/best-seller
+// @desc Retrieve the product with highest rating
+// @access Public
+router.get("/best-seller",async(req, res) => {
+    try{
+        const bestSeller = await Product.findOne().sort({rating: -1});
+        if(bestSeller) {
+            res.json(bestSeller);
+        } else {
+            res.status(404).json({messge: "No best seller found"});
+        }
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error");        
+    }
+});
+
+// @route GET /api/products/new-arrivals
+// @desc Retrieve latest 8 products - Creation date
+// @access Public
+router.get("/new-arrivals", async (req, res) => {
+    try{
+        const newArrivals = await Product.find().sort({createdAt: -1}).limit(8);
+        res.json(newArrivals);
+    }catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+// @route GET /api/products/similar/:id
+// @desc Retrieve similar peoducts based on the current product's gender and category
+// @access Public
+router.get("/similar/:id",async (req, res) => {
+    const {id} = req.params;
+    try{
+        const product = await Product.findById(id);
+        if(!product) {
+            return res.status(404).json({message: "Product Not Found"});
+        }
+        const similarProducts = await Product.find({
+            _id: {$ne :id}, //Exclude the current product ID
+            gender: product.gender,
+            category: product.category
+        }).limit(4);
+
+        res.json(similarProducts);
+    }catch (error){
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+});
+
+// @route GET /api/products/:id
+// @desc Get a single product by ID
+// @access Public
+router.get("/:id", async (req, res) => {
+    try{
+        const product = await Product.findById(req.params.id);
+        if(product) {
+            res.json(product);
+        }else{
+            res.status(404).json({message: "Product Not Found"});
+        }
+    }catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+});
+
+
 
 module.exports = router;
