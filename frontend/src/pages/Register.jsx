dcb4a76d26ddda21fa083ff58d484c5ed9f8d203
racha1125 <akshay25racha@gmail.com>
2025-05-30 12:22:34 +0530

    feat: redux registration and cart merge on register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { registerUser } from '../redux/slices/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { mergeCart } from '../redux/slices/cartSlice';
 function Register() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const {user, guestId} = useSelector((state) => state.auth);
+    const {cart} = useSelector((state) => state.cart);
+
+    // Get redirect parameter and check if it's checkout or somenthing else
+    const redirect = new URLSearchParams(location.search).get('redirect') || "/";
+    const isCheckoutRedirect = redirect.includes("checkout");
+
+    useEffect(() => {
+        if (user){
+            if(cart?.products.length > 0 && guestId) {
+                dispatch(mergeCart({ guestId, user })).then(()=>{
+                    navigate(isCheckoutRedirect ? "/checkout" : "/");
+                });
+            } else{
+                navigate(isCheckoutRedirect ? "/checkout" : "/");
+            }
+        }
+    }, [user, cart, guestId, dispatch, navigate, redirect, isCheckoutRedirect]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("User Registered:", {name, email, password})
+        // console.log("User Registered:", {name, email, password})
+        dispatch(registerUser({ userData: { name, email, password } }))
+            .unwrap()
+            .then((user) => {
+                console.log("Registration successful:", user);
+            })
+            .catch((error) => {
+                console.error("Registration failed:", error);
+            });
     }
   return (
     <div className='flex'>
@@ -53,7 +85,7 @@ function Register() {
                 </button>
                 <p className='mt-6 text-center text-sm'>
                     Already have an account?{""}
-                    <Link to="/login" className='text-blue-500'> Login</Link>
+                    <Link to={`/login${redirect ? `?redirect=${encodeURIComponent(redirect)}` : ''}`} className='text-blue-500'> Login</Link>
                 </p>
             </form>
         </div>
