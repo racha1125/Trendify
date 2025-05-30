d92103f7dce57aa9d4888df7faa125cd72900adc
racha1125 <akshay25racha@gmail.com>
2025-05-30 16:09:18 +0530

    feat: connect ProductManagement to Redux for product list and delete actions

--- frontend/src/components/Admin/ProductManagement.jsx
+++ frontend/src/components/Admin/ProductManagement.jsx
@@ -1,21 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { deleteProduct, fetchAdminProducts } from "../../redux/slices/adminProductSlice";
 
 function ProductManagement() {
-    const products = [
-        {
-            _id: 123123,
-            name: "Shirt",
-            price: 110,
-            stock: 10,
-            sku:"123123123",
-        },
-    ];
+    const dispatch = useDispatch();
+    const {products, loading, error } = useSelector((state) => state.adminProducts);
+
+    useEffect(() => {
+        dispatch(fetchAdminProducts());
+    }, [dispatch]);
+
     const handleDelete = (id) => {
         if(window.confirm("Are you sure you want to delete this product?")) {
             // Call the API to delete the product
-            console.log(`Product with id ${id} deleted`);
+            // console.log(`Product with id ${id} deleted`);
+            dispatch(deleteProduct(id));
         }
     };
+
+    if(loading) {
+        return <div className="text-center text-gray-500">Loading...</div>;
+    }
+    if(error) {
+        return <div className="text-center text-red-500">Error: {error}</div>;
+    }
   return (
     <div className="max-w-7xl mx-auto p-6">
         <h2 className="text-2xl font-bold mb-6">Product Management</h2>
