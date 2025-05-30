e22b5e2195ae712f6e0adefada5fb56e854e8931
racha1125 <akshay25racha@gmail.com>
2025-05-30 16:08:20 +0530

    feat: connect UserManagement to Redux for user CRUD and role management

--- frontend/src/components/Admin/UserManagement.jsx
+++ frontend/src/components/Admin/UserManagement.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUser, deleteUser, fetchUsers, updateUser } from "../../redux/slices/adminSlice";
 
 function UserManagement() {
-    const users = [
-        {
-            _id: 123123,
-            name: "John Doe",
-            email: "john@example.com",
-            role:  "admin",   
-        },
-    ];
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const {user} = useSelector((state) => state.auth);
+    const {users, loading, error} = useSelector((state) => state.admin);
+
+    useEffect(() => {
+        if(user && user.role !== "admin") {
+            navigate("/");
+        }
+    },[user, navigate]);
+
+    useEffect(() => {
+        if(user && user.role === "admin") {
+            dispatch(fetchUsers());
+        }
+    }, [dispatch, user]);
+
     const [formData, setFormData]= useState({
         name: "",
         email: "",
@@ -23,7 +36,10 @@ function UserManagement() {
     };
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Form submitted", formData);
+        // console.log("Form submitted", formData);
+        dispatch(addUser(formData));
+
+
         // Reset the form after submission
         setFormData({
             name: "",
@@ -33,16 +49,20 @@ function UserManagement() {
         })
     };
     const handleRoleChange = (userId, newRole) => {
-        console.log({id: userId, role: newRole});
+        // console.log({id: userId, role: newRole});
+        dispatch(updateUser({id: userId, role: newRole}));
     };
     const handleDeleteUser = (userId) => {
         if(window.confirm("Are you sure you want to delete this user?")) {
-            console.log("User deleted", userId);
+            // console.log("User deleted", userId);
+            dispatch(deleteUser(userId));
         }
     };
   return (
     <div className="max-w-7xl mx-auto p-6">
         <h2 className="text-2xl font-bold mb-4">User Management</h2>
+        {loading && <p>Loading users...</p>}
+        {error && <p className="text-red-500">Error: {error}</p>}
         {/* Add new user form */}
         <div className="p-6 rounded-lg mb-6">
             <h3 className="text-lg font-bold mb-4">Add New User</h3>
